eaf8d37a289f23996af099223f3253e988f1376d
RobinBastiaan <robinbastiaan@gmail.com>
2024-12-26 16:58:55 +0100

    :seedling: Add initial item extraction as seed with orangehill/iseed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Add initial item extraction as seed with orangehill/iseed.
+        $this->call(ExtractedItemsTableSeeder::class);
     }
 }
