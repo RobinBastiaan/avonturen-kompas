3d192aabe46eb920579666744971b54f2341d983
RobinBastiaan <robinbastiaan@gmail.com>
2025-02-01 13:59:17 +0100

    :hammer: Schedule item extraction and processing monthly

---
+++ app/Console/Kernel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console;
+
+use App\Console\Commands\ExtractItemsCommand;
+use App\Console\Commands\ProcessItemsCommand;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * These commands may be run in a single run of the scheduler.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        $schedule->command(ExtractItemsCommand::class)
+            ->monthly()
+            ->then(function () {
+                Artisan::call(ProcessItemsCommand::class);
+            });
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
